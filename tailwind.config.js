854eece11c011f0d6578af57e6410e5790f2b7e3
iamabhijitmishra <theabhimishra.org@gmail.com>
2025-01-29 17:59:05 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'netflix-red': '#E50914',
+        'netflix-dark': '#221f1f',
+        'netflix-light': '#f5f5f1',
+      },
+    },
+  },
+  plugins: [],
+}
